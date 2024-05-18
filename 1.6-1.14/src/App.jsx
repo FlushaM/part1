5a50356efb0ba669c71ef9db6b3bdc0a4a9c1041
FlushaM <83796763+FlushaM@users.noreply.github.com>
2024-05-18 17:10:14 -0400

    1.7 done

--- 1.6-1.14/src/App.jsx
+++ 1.6-1.14/src/App.jsx
@@ -4,6 +4,11 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+
+  const total = good + neutral + bad;
+  const promedio = (good - bad) / total || 0;
+  const porcentajePositivo = (good/total) * 100 || 0;
+
   return (
     <div>
       <h1>Give Feedback</h1>
@@ -14,6 +19,9 @@ const App = () => {
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>all: {total}</p>
+      <p>Media: {promedio.toFixed(2)}</p>
+      <p>PorcentajePositivo: {porcentajePositivo.toFixed(2)}%</p>
     </div>
   );
 };
