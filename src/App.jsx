fd02df88e327a98fe2233bdbb6b0b80f6c53df36
FlushaM <83796763+FlushaM@users.noreply.github.com>
2024-05-18 13:23:42 -0400

    Props
    usamos props

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-const Hello = () => {
+const Hello = (props) => {
+  console.log(props)
   return (
     <div>
-      <p>Hello world</p>
+      <p>Hello world {props.name}, you are {props.age} years old</p>
     </div>
   )
 }
 
 const App = () =>{
+  const name = "peter"
+  const age = 10
+
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello/>
-      <Hello/>
-      <Hello/>
+      <Hello name = "Maya" age ={26 + 10}/>
+      <Hello name = {name} age={age}/>
     </div>
   )
 }
