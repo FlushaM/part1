76d54190d97b68e4d928b0e9b4d9381b79e172cf
FlushaM <83796763+FlushaM@users.noreply.github.com>
2024-05-18 14:34:17 -0400

    1.3 al 1.5

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-const Header = ({course}) => {
-  return <h1>{course}</h1>;
-};
 
-const Content = ({parts}) =>{
-  return (
-    <div>
-      {parts.map((part, index) => (
-        <p key={index}>
-          {part.name} {part.exercises}
-        </p>
-      ))}
-    </div>
-  );
-};
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map((part, index) => (
+      <p key={index}>
+        {part.name} {part.exercises}
+      </p>
+    ))}
+  </div>
+);
 
 const Total = ({ parts }) => {
   const total = parts.reduce((sum, part) => sum + part.exercises, 0);
@@ -20,21 +17,20 @@ const Total = ({ parts }) => {
 };
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
+  const course = {name: 'Half Stack application development',parts:[
     { name: 'Fundamentals of React', exercises: 10 },
     { name: 'Using props to pass data', exercises: 7 },
     { name: 'State of a component', exercises: 14 },
-  ];
+  ]};
 
 
   return (
     
     <div>
       <div>
-        <Header course={course}/>
-        <Content parts={parts}/>
-        <Total parts={parts} />
+        <Header course={course.name}/>
+        <Content parts={course.parts}/>
+        <Total parts={course.parts} />
       </div>
     </div>
   )
