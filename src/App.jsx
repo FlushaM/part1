4409b6cc5395a7665b332ffb92f0f5673648d031
FlushaM <83796763+FlushaM@users.noreply.github.com>
2024-05-18 14:06:15 -0400

    Ejercicios
    eJERCICOS 1.1 1.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,41 @@
-const Hello = (props) => {
-  console.log(props)
+const Header = ({course}) => {
+  return <h1>{course}</h1>;
+};
+
+const Content = ({parts}) =>{
   return (
     <div>
-      <p>Hello world {props.name}, you are {props.age} years old</p>
+      {parts.map((part, index) => (
+        <p key={index}>
+          {part.name} {part.exercises}
+        </p>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-const App = () =>{
-  const name = "peter"
-  const age = 10
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return <p>Number of exercises {total}</p>;
+};
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 },
+  ];
+
 
   return (
+    
     <div>
-      <h1>Greetings</h1>
-      <Hello name = "Maya" age ={26 + 10}/>
-      <Hello name = {name} age={age}/>
+      <div>
+        <Header course={course}/>
+        <Content parts={parts}/>
+        <Total parts={parts} />
+      </div>
     </div>
   )
 }
