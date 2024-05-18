985fe3f27dbf5ef5d655030b03fe587517f589c5
FlushaM <83796763+FlushaM@users.noreply.github.com>
2024-05-18 17:17:29 -0400

    1.8-1.9

--- 1.6-1.14/src/App.jsx
+++ 1.6-1.14/src/App.jsx
@@ -1,13 +1,31 @@
-import { useState } from "react"
+import { useState } from 'react';
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = (good - bad) / total || 0;
+  const positivePercentage = (good / total) * 100 || 0;
+
+  if (total === 0) {
+    return <p>No feedback given</p>;
+  }
+
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>All: {total}</p>
+      <p>Average: {average.toFixed(2)}</p>
+      <p>Positive: {positivePercentage.toFixed(2)}%</p>
+    </div>
+  );
+};
 
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
-  const total = good + neutral + bad;
-  const promedio = (good - bad) / total || 0;
-  const porcentajePositivo = (good/total) * 100 || 0;
 
   return (
     <div>
@@ -15,17 +33,9 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>Good</button>
       <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
       <button onClick={() => setBad(bad + 1)}>Bad</button>
-      <h2>Statistics</h2>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>all: {total}</p>
-      <p>Media: {promedio.toFixed(2)}</p>
-      <p>PorcentajePositivo: {porcentajePositivo.toFixed(2)}%</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
 
-
-
-export default App
+export default App;
