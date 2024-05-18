e5342481ac922d88cd89c1a4f50ff00a1d081bb8
FlushaM <83796763+FlushaM@users.noreply.github.com>
2024-05-18 17:39:08 -0400

    1.14 done

--- 1.6-1.14/src/App.jsx
+++ 1.6-1.14/src/App.jsx
@@ -1,52 +1,42 @@
+
 import { useState } from 'react';
 
-const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>;
-
-const StatisticLine = ({ text, value }) => (
-  <tr>
-    <td>{text}</td>
-    <td>{value}</td>
-  </tr>
-);
-
-const Statistics = ({ good, neutral, bad }) => {
-  const total = good + neutral + bad;
-  const average = (good - bad) / total || 0;
-  const positivePercentage = (good / total) * 100 || 0;
-
-  if (total === 0) {
-    return <p>No feedback given</p>;
-  }
-
-  return (
-    <div>
-      <h2>Statistics</h2>
-      <table>
-        <tbody>
-          <StatisticLine text="Good" value={good} />
-          <StatisticLine text="Neutral" value={neutral} />
-          <StatisticLine text="Bad" value={bad} />
-          <StatisticLine text="All" value={total} />
-          <StatisticLine text="Average" value={average.toFixed(2)} />
-          <StatisticLine text="Positive" value={`${positivePercentage.toFixed(2)}%`} />
-        </tbody>
-      </table>
-    </div>
-  );
-};
-
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const anecdotes = [
+    "Recuerdo cuando era pequeño y era muy fan de cirstiano ronaldo pero nunca pude ser como el ",
+    "ya que era muy malo para el futbol"
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
+
+  const handleVote = () => {
+    const newVotes = [...votes];
+    newVotes[selected] += 1;
+    setVotes(newVotes);
+  };
+
+  const handleNextAnecdote = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomIndex);
+  };
+
+  const mostVotedIndex = votes.indexOf(Math.max(...votes));
 
   return (
     <div>
-      <h1>Give Feedback</h1>
-      <Button handleClick={() => setGood(good + 1)} text="Good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="Bad" />
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={handleVote}>Vote</button>
+      <button onClick={handleNextAnecdote}>Next Anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      {votes[mostVotedIndex] > 0 && (
+        <div>
+          <p>{anecdotes[mostVotedIndex]}</p>
+          <p>has {votes[mostVotedIndex]} votes</p>
+        </div>
+      )}
     </div>
   );
 };
